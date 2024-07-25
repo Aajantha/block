7805c1f65b9f7b7cea468236a1aac51c1a2327f0
Aajantha <ajanth663@gmil.com>
2024-07-25 04:06:16 +0000

    s8

--- src/components/Post3.js
+++ src/components/Post3.js
@@ -6,7 +6,7 @@ function Post3() {
     <div id="post3" className="post">
       <h2>Algorithm</h2>
       <img src={image3} alt="Post 3" />
-      <p>An algorithm is a set of commands that must be followed for  to perform calculations or other problem-solving operations.According to its formal definition, an algorithm is a finite set of instructions carried out in a specific order to perform a particular task.</p>
+      <p>An algorithm is a set of commands that must  followed for  to perform calculations or other problem-solving operations.According to its formal definition, an algorithm is a finite set of instructions carried out in a specific order to perform a particular task.</p>
       <button>Read More</button>
     </div>
   );
