de012b20db801e4759bf21b526828c1041060dea
Aajantha <ajanth663@gmil.com>
2024-07-25 03:52:45 +0000

    s3

--- src/components/Post1.js
+++ src/components/Post1.js
@@ -6,7 +6,7 @@ function Post1() {
     <div id="post1" className="post">
       <h2>JavaScript</h2>
       <img src={image1} alt="JavaScript" />
-      <p>JavaScript is a versatile programming language JavaScript, often abbreviated as JS, is a programming language and core technology of the Web, alongside HTML and CSS. 99% of websites use JavaScript on the client side for webpage behavior. Web browsers have a dedicated JavaScript engine that executes the client code.</p>
+      <p>JavaScript is a versatile programming language JavaScript, often abbreviated as JS, a programming language and core technology of the Web, alongside HTML and CSS. 99% of websites use JavaScript on the client side for webpage behavior. Web browsers have a dedicated JavaScript engine that executes the client code.</p>
       <button>Read More</button>
     </div>
   );
